b92506191cf2c2b156cfc010b23d009495d54373
Hari <hari.kulhari@ranosys.com>
2020-02-14 20:08:11 +0530

    Initial commit

---
+++ lib/ui/main/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/ui/categories/categories_screen.dart';
+import 'package:meal_app/ui/main/tabs_screen.dart';
+import 'package:meal_app/ui/mealDetail/meal_detail_screen.dart';
+import 'package:meal_app/ui/meals/meals_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meal App',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline5: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      initialRoute: "/",
+      routes: <String, WidgetBuilder>{
+        '/': (context) => TabsScreen(),
+        MealsScreen.ROUT: (context) => MealsScreen(),
+        MealDetailScreen.ROUT: (context) => MealDetailScreen(),
+      },
+    );
+  }
+}
